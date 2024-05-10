1b028217cd95469507f1a9b95295de908cfc11a3
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-10 19:25:28 +0800

    Animate the text in Feedback, unfinished

--- src/Feedback.tsx
+++ src/Feedback.tsx
@@ -1,11 +1,46 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+const feedbacks = [
+  { text: "THE BEST BURGER I'VE EVER HAD. -AANG" },
+  { text: "FLAVOR TOWN! -SOKKA" },
+];
 export const Feedback = () => {
+  const duplicatedFeedbacks = [...feedbacks, ...feedbacks];
+
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    // Set the window width after the component mounts
+    setWindowWidth(window.innerWidth);
+  }, []);
+
   return (
-    <section className="flex flex-col gap-16 font-bowlby">
-      <div className="relative h-24 w-full">
-        <h1 className="absolute z-20 text-8xl text-accentLime">
-          “ THE BEST BURGER “
-        </h1>
-      </div>
+    <section className="flex flex-col gap-8 font-bowlby">
+      <motion.div
+        className="flex"
+        animate={{
+          x: [windowWidth, -windowWidth * 2],
+          transition: {
+            x: {
+              repeat: Infinity,
+              repeatType: "loop",
+              duration: 10,
+              ease: "linear",
+            },
+          },
+        }}
+      >
+        {duplicatedFeedbacks.map((text, index) => (
+          <div className="flex" key={index}>
+            <motion.h1 className="whitespace-nowrap text-8xl text-accentLime">
+              “ {text.text} “
+            </motion.h1>
+          </div>
+        ))}
+        {/* <motion.h1 className="whitespace-nowrap text-8xl text-accentLime">
+          “ THE BEST BURGER I'VE EVER HAD “
+        </motion.h1> */}
+      </motion.div>
       <div
         className="flex h-[503px] items-end rounded-3xl border-4 border-textBeige bg-cover bg-center p-4"
         style={{
