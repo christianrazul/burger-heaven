d73377df8dcc1336db317b5fb42c722e9f212ea3
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-10 11:59:06 +0800

    Add the squiggly lines to CTA

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "./Navbar";
 
 function App() {
   return (
-    <div className="flex w-screen flex-col gap-8 px-10 py-6 font-bowlby 2xl:px-[620px]">
+    <div className="flex w-full flex-col gap-8 overflow-hidden px-10 py-6 font-bowlby 2xl:px-[620px]">
       <Navbar />
       <Hero />
       <MenuPreview />
--- src/CTASection.tsx
+++ src/CTASection.tsx
@@ -1,6 +1,10 @@
 export const CTASection = () => {
   return (
-    <section className="my-24 flex h-fit w-full flex-col items-center justify-center">
+    <section className="relative flex h-[503px] w-full flex-col items-center justify-center">
+      <img
+        src="./src/assets/right-squiggly.svg"
+        className="absolute -left-[520px] -top-28"
+      />
       <div className="flex flex-col items-center gap-8">
         <h1 className="w-[895px] text-center text-7xl text-textBeige">
           ORDER ONLINE OR COME VISIT US TODAY
@@ -9,6 +13,10 @@ export const CTASection = () => {
           GET STARTED
         </button>
       </div>
+      <img
+        src="./src/assets/left-squiggly.svg"
+        className="absolute -bottom-16 -right-[520px] z-10"
+      />
     </section>
   );
 };
