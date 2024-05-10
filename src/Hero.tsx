d7a0d619c48589edd7987055b1c7fb3c646abd94
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-10 16:33:28 +0800

    Add initial animations to hero

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,12 +1,73 @@
+import { motion } from "framer-motion";
 import { BurgerImg } from "./BurgerImg";
 
 export const Hero = () => {
+  const textAnimation = {
+    hidden: { y: 30, opacity: 0 },
+    visible: (index: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: index * 0.1,
+        duration: 0.6,
+        type: "spring",
+        stiffness: 120,
+        damping: 12,
+      },
+    }),
+  };
+
+  const containerVariants = {
+    hidden: { scale: 0 },
+    visible: {
+      scale: 1,
+      transition: {
+        type: "spring", // Using a spring type transition
+        stiffness: 260, // Spring stiffness
+        damping: 20, // Damping for the spring
+        mass: 1, // Mass of the element being animated
+      },
+    },
+  };
+
   return (
     <div className="flex w-full flex-col items-center">
-      <h1 className="w-[1158px] text-center font-bowlby text-8xl leading-[88.9%] text-textBeige">
+      {/* <h1 className="w-[1158px] text-center font-bowlby text-8xl leading-[88.9%] text-textBeige">
         NEW YORK'S FAVORITE ORGANIC HAMBURGER JOINT
-      </h1>
-      <div className="relative flex h-[478px] w-full items-center justify-center">
+      </h1> */}
+      <motion.h1
+        className="font-bowlby text-8xl leading-[88.9%] text-textBeige"
+        variants={textAnimation}
+        initial="hidden"
+        animate="visible"
+        custom={0}
+      >
+        NEW YORK'S
+      </motion.h1>
+      <motion.h1
+        className="font-bowlby text-8xl leading-[88.9%] text-textBeige"
+        variants={textAnimation}
+        initial="hidden"
+        animate="visible"
+        custom={1}
+      >
+        FAVORITE ORGANIC
+      </motion.h1>
+      <motion.h1
+        className="font-bowlby text-8xl leading-[88.9%] text-textBeige"
+        variants={textAnimation}
+        initial="hidden"
+        animate="visible"
+        custom={2}
+      >
+        HAMBURGER JOINT
+      </motion.h1>
+      <motion.div
+        className="relative flex h-[478px] w-full items-center justify-center"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
         <img src="./src/assets/left-squiggly.svg" className="absolute left-0" />
         <div className="relative -top-6 h-full w-4/5">
           <BurgerImg
@@ -26,7 +87,7 @@ export const Hero = () => {
           src="./src/assets/right-squiggly.svg"
           className="absolute right-0"
         />
-      </div>
+      </motion.div>
       <div className="flex h-96 w-full rounded-xl border-4 border-bgDarkBlue bg-accentLime px-24 py-20">
         <h1 className="font-bowlby text-7xl">THE BURGER ABOVE ALL BURGERS</h1>
         <div className="flex w-full flex-col justify-between gap-8">
