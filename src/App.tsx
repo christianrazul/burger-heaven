f751bf528904c3a89a2cacdf55a02a1c00ce3178
Rudolph Christian Razul <razulchristian@gmail.com>
2024-05-11 20:02:14 +0800

    Move components into components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { CTASection } from "./CTASection";
-import { Feedback } from "./Feedback";
-import { Footer } from "./Footer";
-import { Hero } from "./Hero";
-import { MenuPreview } from "./MenuPreview";
-import { Navbar } from "./Navbar";
+import { CTASection } from "./components/CTASection";
+import { Feedback } from "./components/Feedback";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { MenuPreview } from "./components/MenuPreview";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
